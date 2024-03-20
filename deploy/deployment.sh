ba3832c2086d8f05914174a4335e96c173c9c8fe
Chris Scott <chris@chrisdjscott.co.uk>
2024-03-21 09:58:40 +1300

    working on automating deployment

--- deploy/deployment.sh
+++ deploy/deployment.sh
@@ -2,13 +2,16 @@
 
 case $1 in
     "destroy")
-        ansible-playbook provision-infrastructure.yml -e operation=destroy -e terraform_workspace=${2:-test}
+        ansible-playbook provision-infrastructure.yml -e operation=destroy -e terraform_workspace=${2:-dev}
         ;;
 
     "create")
-        ansible-playbook provision-infrastructure.yml -e operation=create -e terraform_workspace=${2:-test}
+        # TODO: need to create workspace in case it doesn't exist
+
+        ansible-playbook provision-infrastructure.yml -e operation=create -e terraform_workspace=${2:-dev}
         ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i host.ini setup-backend.yml \
-            -u ${TF_VAR_vm_user} --key-file '${TF_VAR_key_file}' -e terraform_workspace=${2:-test}
+            -u ${TF_VAR_vm_user} --key-file '${TF_VAR_key_file}' -e terraform_workspace=${2:-dev} \
+            -e @vars/${2:-dev}.yml
         ;;
 
     *)
