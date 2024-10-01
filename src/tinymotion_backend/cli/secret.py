bd3182cc9e442703a10e2a79f81fc97f8d153cde
Chris Scott <chris@chrisdjscott.co.uk>
2024-10-02 10:03:11 +1300

    update secrets cli tool

--- src/tinymotion_backend/cli/secret.py
+++ src/tinymotion_backend/cli/secret.py
@@ -11,36 +11,68 @@ def secret():
 
 
 @secret.command()
-@click.option("-a", "--auth", is_flag=True, default=False, help="Generate access and refresh tokens")
-@click.option("-d", "--database", is_flag=True, default=False, help="Generate database secrets")
-@click.option("-v", "--video", is_flag=True, default=False, help="Generate video secrets")
-def generate(auth: bool, database: bool, video: bool):
+@click.option("-a", "--access", is_flag=True, default=False, help="Generate access token")
+@click.option("-r", "--refresh", is_flag=True, default=False, help="Generate refresh token")
+@click.option("-d", "--database", is_flag=True, default=False, help="Generate database secret")
+@click.option("-v", "--video", is_flag=True, default=False, help="Generate video secret")
+@click.option("-p", "--plain", is_flag=True, default=False, help="Just print the secret with no extra text")
+def generate(access: bool, refresh: bool, database: bool, video: bool, plain: bool):
     """Generate new secrets"""
-    if not (auth or database or video):
+    # at least one secret should be specified
+    if not (access or refresh or database or video):
         click.echo("Must select which secrets to generate")
         click.echo("")
         ctx = click.get_current_context()
         click.echo(ctx.get_help())
         ctx.exit()
 
-    if auth:
+    # if plain was chosen, only one secret should be specified
+    if plain and (access + refresh + database + video > 1):
+        click.echo("Must select only one secret to generate when choosing \"--plain\"")
         click.echo("")
-        click.echo("Generating access and refresh tokens:")
+        ctx = click.get_current_context()
+        click.echo(ctx.get_help())
+        ctx.exit()
+
+    if access:
+        if not plain:
+            click.echo("")
+            click.echo("Generating access token:")
         access_token = secrets.token_urlsafe()
+        if plain:
+            click.echo(access_token)
+        else:
+            click.echo(f"access_token = {access_token}")
+
+    if refresh:
+        if not plain:
+            click.echo("")
+            click.echo("Generating refresh token:")
         refresh_token = secrets.token_urlsafe()
-        click.echo(f"access_token = {access_token}")
-        click.echo(f"refresh_token = {refresh_token}")
+        if plain:
+            click.echo(refresh_token)
+        else:
+            click.echo(f"refresh_token = {refresh_token}")
 
     if database:
-        click.echo("")
-        click.echo("Generating database secrets:")
+        if not plain:
+            click.echo("")
+            click.echo("Generating database secrets:")
         database_secret = secrets.token_urlsafe()
-        click.echo(f"database_secret = {database_secret}")
+        if plain:
+            click.echo(database_secret)
+        else:
+            click.echo(f"database_secret = {database_secret}")
 
     if video:
-        click.echo("")
-        click.echo("Generating video secrets:")
+        if not plain:
+            click.echo("")
+            click.echo("Generating video secrets:")
         video_secret = Fernet.generate_key().decode("ascii")
-        click.echo(f"video_secret = {video_secret}")
+        if plain:
+            click.echo(video_secret)
+        else:
+            click.echo(f"video_secret = {video_secret}")
 
-    click.echo("")
+    if not plain:
+        click.echo("")
