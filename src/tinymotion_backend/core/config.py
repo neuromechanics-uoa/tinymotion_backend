02fdea8cca0b8fe40b5783c067cc93ff583f1062
Chris Scott <chris@chrisdjscott.co.uk>
2024-10-02 10:40:10 +1300

    secrets default to None if not set

--- src/tinymotion_backend/core/config.py
+++ src/tinymotion_backend/core/config.py
@@ -2,8 +2,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    ACCESS_TOKEN_SECRET_KEY: str
-    REFRESH_TOKEN_SECRET_KEY: str
+    ACCESS_TOKEN_SECRET_KEY: str | None = None
+    REFRESH_TOKEN_SECRET_KEY: str | None = None
 
     ALGORITHM: str = "HS256"
 
@@ -16,10 +16,10 @@ class Settings(BaseSettings):
     FILE_CHUNK_SIZE_BYTES: int = 1024 * 1024 * 10  # default to 10 MB
 
     DATABASE_URI: str = "sqlite:///tinymotion.db"
-    DATABASE_SECRET_KEY: str
+    DATABASE_SECRET_KEY: str | None = None
 
     VIDEO_LIBRARY_PATH: str = "./videos"
-    VIDEO_SECRET_KEY: str
+    VIDEO_SECRET_KEY: str | None = None
 
 
     model_config = SettingsConfigDict(case_sensitive=True, env_prefix="TINYMOTION_", env_file=".tinymotion.env")
